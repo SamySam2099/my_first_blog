e0233e6081bb7a9862bb3fa42e2f741bc94f7bf7
Samuel <sxi983@student.bham.ac.uk>
2020-07-23 19:43:21 +0100

    Changed the syntax of wsgi for the site

--- my_site/my_site/wsgi.py
+++ my_site/my_site/wsgi.py
@@ -7,10 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_site.settings')
-
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
